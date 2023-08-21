ccfbf12ab3c1a632300ece0a9065908300b86243
karneje1992 <karneje.1992@gmail.com>
2023-08-21 14:14:11 +0300

    setup
    new project

---
+++ SwiftUITestProject/SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by Rostyslav Didenko on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
